7e8894cb09ccae621e031c98ba8c5428c370df6d
rohanChavan21 <rohanchavank22@gmail.com>
2023-09-15 10:27:17 +0530

    Assignment 04 Done

--- Assignment 04/playFairCipher.cpp
+++ Assignment 04/playFairCipher.cpp
@@ -1,173 +1,197 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-// Function to generate the Playfair key matrix from the key
-vector<vector<char>> generateKeyMatrix(const string& key) {
-    string alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; // Exclude 'J'
+class PlayfairCipher {
+  public:
+    static pair<vector<vector<char>>, unordered_map<char, pair<int, int>>>
+    getKeyMatrixAndPositions(const string &key) {
+        vector<vector<char>> keyMatrix(5, vector<char>(5));
+        int i = 0, j = 0;
+        unordered_set<char> set;
+        unordered_map<char, pair<int, int>> position;
 
-    // Remove duplicates from the key and append the alphabet
-    string keyWithoutDuplicates = "";
-    for (char c : key) {
-        if (keyWithoutDuplicates.find(c) == string::npos) {
-            keyWithoutDuplicates += c;
-        }
-    }
-    string combinedKey = keyWithoutDuplicates + alphabet;
+        for (char c : key) {
+            if (c == 'j')
+                c = 'i';
 
-    // Create a 5x5 key matrix
-    vector<vector<char>> keyMatrix(5, vector<char>(5));
+            if (set.find(c) != set.end())
+                continue;
 
-    int index = 0;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            keyMatrix[i][j] = combinedKey[index++];
-        }
-    }
+            set.insert(c);
+            keyMatrix[i][j] = c;
+            position[c] = {i, j};
 
-    return keyMatrix;
-}
-
-// Function to encrypt a message using the Playfair cipher
-string encryptPlayfairCipher(const string& message, const string& key) {
-    vector<vector<char>> keyMatrix = generateKeyMatrix(key);
-    string plaintext = message;
-    string ciphertext = "";
-
-    // Remove spaces and convert to uppercase
-    plaintext.erase(remove_if(plaintext.begin(), plaintext.end(), ::isspace), plaintext.end());
-    transform(plaintext.begin(), plaintext.end(), plaintext.begin(), ::toupper);
-
-    // Replace 'J' with 'I' in the plaintext
-    replace(plaintext.begin(), plaintext.end(), 'J', 'I');
-
-    int messageLength = plaintext.length();
-    int index = 0;
-
-    while (index < messageLength) {
-        char first = plaintext[index];
-        char second = (index + 1 < messageLength) ? plaintext[index + 1] : 'X'; // Use 'X' as filler if needed
-
-        int row1, col1, row2, col2;
-
-        // Find the positions of the letters in the key matrix
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (keyMatrix[i][j] == first) {
-                    row1 = i;
-                    col1 = j;
-                }
-                if (keyMatrix[i][j] == second) {
-                    row2 = i;
-                    col2 = j;
-                }
+            j++;
+            if (j == 5) {
+                j = 0;
+                i++;
             }
         }
 
-        char encryptedFirst, encryptedSecond;
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (c == 'j')
+                continue;
 
-        // Apply the Playfair rules to encrypt the digram
-        if (row1 == row2) {
-            // Same row, shift right
-            encryptedFirst = keyMatrix[row1][(col1 + 1) % 5];
-            encryptedSecond = keyMatrix[row2][(col2 + 1) % 5];
-        } else if (col1 == col2) {
-            // Same column, shift down
-            encryptedFirst = keyMatrix[(row1 + 1) % 5][col1];
-            encryptedSecond = keyMatrix[(row2 + 1) % 5][col2];
-        } else {
-            // Forming a rectangle, swap horizontally
-            encryptedFirst = keyMatrix[row1][col2];
-            encryptedSecond = keyMatrix[row2][col1];
-        }
+            if (set.find(c) != set.end())
+                continue;
 
-        ciphertext += encryptedFirst;
-        ciphertext += encryptedSecond;
+            set.insert(c);
+            keyMatrix[i][j] = c;
+            position[c] = {i, j};
 
-        index += 2;
-    }
-
-    return ciphertext;
-}
-
-// Function to decrypt a message encrypted with the Playfair cipher
-string decryptPlayfairCipher(const string& ciphertext, const string& key) {
-    vector<vector<char>> keyMatrix = generateKeyMatrix(key);
-    string decryptedMessage = "";
-
-    int ciphertextLength = ciphertext.length();
-    int index = 0;
-
-    while (index < ciphertextLength) {
-        char first = ciphertext[index];
-        char second = ciphertext[index + 1];
-
-        int row1, col1, row2, col2;
-
-        // Find the positions of the letters in the key matrix
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (keyMatrix[i][j] == first) {
-                    row1 = i;
-                    col1 = j;
-                }
-                if (keyMatrix[i][j] == second) {
-                    row2 = i;
-                    col2 = j;
-                }
+            j++;
+            if (j == 5) {
+                j = 0;
+                i++;
             }
         }
 
-        char decryptedFirst, decryptedSecond;
+        position[j] = position[i];
 
-        // Apply the Playfair rules to decrypt the digram
-        if (row1 == row2) {
-            // Same row, shift left
-            decryptedFirst = keyMatrix[row1][(col1 + 4) % 5];
-            decryptedSecond = keyMatrix[row2][(col2 + 4) % 5];
-        } else if (col1 == col2) {
-            // Same column, shift up
-            decryptedFirst = keyMatrix[(row1 + 4) % 5][col1];
-            decryptedSecond = keyMatrix[(row2 + 4) % 5][col2];
-        } else {
-            // Forming a rectangle, swap horizontally
-            decryptedFirst = keyMatrix[row1][col2];
-            decryptedSecond = keyMatrix[row2][col1];
+        return {keyMatrix, position};
+    }
+
+    static vector<string> getDiagrams(const string &text) {
+        int n = text.size();
+        int i = 0;
+        vector<string> diagrams;
+
+        while (i + 1 < n) {
+            if (text[i] != text[i + 1]) {
+                string d;
+                d += tolower(text[i]);
+                d += tolower(text[i + 1]);
+                diagrams.push_back(d);
+
+                i += 2;
+            } else {
+                string d;
+                d += tolower(text[i]);
+                d += 'x';
+                diagrams.push_back(d);
+
+                i++;
+            }
         }
 
-        decryptedMessage += decryptedFirst;
-        decryptedMessage += decryptedSecond;
+        if (i == n - 1) {
+            string d;
+            d += tolower(text[i]);
+            d += 'x';
+            diagrams.push_back(d);
+        }
 
-        index += 2;
+        return diagrams;
     }
 
-    // Remove any trailing 'X' fillers
-    size_t pos = decryptedMessage.find_last_not_of('X');
-    if (pos != string::npos) {
-        decryptedMessage = decryptedMessage.substr(0, pos + 1);
+    static string encrypt(const string &plaintext, const string &key) {
+        auto p = getKeyMatrixAndPositions(key);
+        auto keyMatrix = p.first;
+        auto position = p.second;
+        vector<string> diagrams = getDiagrams(plaintext);
+        stringstream ciphertext;
+
+        for (string &diagram : diagrams) {
+            auto p1 = position[diagram[0]];
+            auto p2 = position[diagram[1]];
+            auto i0 = p1.first, j0 = p1.second;
+            auto i1 = p2.first, j1 = p2.second;
+
+            if (i0 == i1) {
+                diagram[0] = keyMatrix[i0][(j0 + 1) % 5];
+                diagram[1] = keyMatrix[i0][(j1 + 1) % 5];
+            } else if (j0 == j1) {
+                diagram[0] = keyMatrix[(i0 + 1) % 5][j0];
+                diagram[1] = keyMatrix[(i1 + 1) % 5][j0];
+            } else {
+                diagram[0] = keyMatrix[i0][j1];
+                diagram[1] = keyMatrix[i1][j0];
+            }
+
+            ciphertext << diagram;
+        }
+
+        string answer = ciphertext.str();
+
+        transform(answer.begin(), answer.end(), answer.begin(), ::toupper);
+        return answer;
     }
 
-    return decryptedMessage;
-}
+    static string decrypt(const string &ciphertext, const string &key) {
+        auto p = getKeyMatrixAndPositions(key);
+        auto keyMatrix = p.first;
+        auto position = p.second;
 
+        vector<string> diagrams = getDiagrams(ciphertext);
+        stringstream plaintext;
+
+        for (string &diagram : diagrams) {
+            auto p1 = position[diagram[0]];
+            auto p2 = position[diagram[1]];
+            auto i0 = p1.first, j0 = p1.second;
+            auto i1 = p2.first, j1 = p2.second;
+
+            if (i0 == i1) {
+                diagram[0] = keyMatrix[i0][(j0 - 1 + 5) % 5];
+                diagram[1] = keyMatrix[i0][(j1 - 1 + 5) % 5];
+            } else if (j0 == j1) {
+                diagram[0] = keyMatrix[(i0 - 1 + 5) % 5][j0];
+                diagram[1] = keyMatrix[(i1 - 1 + 5) % 5][j0];
+            } else {
+                diagram[0] = keyMatrix[i0][j1];
+                diagram[1] = keyMatrix[i1][j0];
+            }
+
+            plaintext << diagram;
+        }
+
+        return plaintext.str();
+    }
+};
 
 int main() {
-    string message, key;
+    cout << "PlayFair Cipher:\n"
+         << "Enter your choice:\n"
+         << "1. Encrypt\n"
+         << "2. Decrypt\n";
 
-    cout << "Enter a message: ";
-    getline(cin, message);
+    int choice;
+    cin >> choice;
 
-    cout << "Enter the encryption key: ";
-    cin >> key;
+    switch (choice) {
+    case 1: {
+        cout << "Enter plaintext: ";
+        string plaintext;
+        cin.get();
+        getline(cin, plaintext);
+        plaintext.erase(remove_if(plaintext.begin(), plaintext.end(), ::isspace),
+                        plaintext.end());
 
-    string encryptedMessage = encryptPlayfairCipher(message, key);
-    string decryptedMessage = decryptPlayfairCipher(encryptedMessage, key);
+        cout << "Enter key : ";
+        string key;
+        cin >> key;
 
-    cout << "Encrypted message: " << encryptedMessage << endl;
-    cout << "Decrypted message: " << decryptedMessage << endl;
+        string ciphertext = PlayfairCipher::encrypt(plaintext, key);
+
+        cout << "Plaintext:  " << plaintext << "\n"
+             << "Ciphertext: " << ciphertext << "\n";
+    } break;
+
+    case 2: {
+        cout << "Enter ciphertext: ";
+        string ciphertext;
+        cin >> ciphertext;
+
+        cout << "Enter key : ";
+        string key;
+        cin >> key;
+
+        string plaintext = PlayfairCipher::decrypt(ciphertext, key);
+
+        cout << "Ciphertext: " << ciphertext << "\n"
+             << "Plaintext:  " << plaintext << "\n";
+    } break;
+    }
 
     return 0;
 }
